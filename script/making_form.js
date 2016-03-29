41aa1083868b938cd033f4581dccd2c8f28e0f9f
Julian Kiedaisch <julian.kiedaisch@googlemail.com>
2016-03-29 19:34:33 +0200

    Beertyps can be added

--- script/making_form.js
+++ script/making_form.js
@@ -1,9 +1,5 @@
 var mySelect = $('#select_biertypen1');
-var myOptions = {
-    1 : 'Pils',
-    2 : 'Weizen',
-		3 : 'Brown Ale'
-};
+var myOptions = ['Pils', 'Weizen', 'Brown Ale'];
 
 /* Biertypen bearbeiten einblenden */
 $('#button_biertyp1').click(function () {
@@ -15,10 +11,8 @@ $('#button_biertyp1').click(function () {
 $('#button_biertyp2').click(function () {
 		$('#div_biertyp1').show("slow");
 		$('#div_biertyp2').hide("slow");
-		a = Object.keys(myOptions).length + 1;
-		$('#biertyp_message').text(a);
-		myOptions.a = $('#neu_biertyp_name').val();
-		mySelect.append($('<option></option>').val(a).html($('#neu_biertyp_name').val()));
+		myOptions.push($('#neu_biertyp_name').val());
+		mySelect.append($('<option></option>').val($('#neu_biertyp_name').val()).html($('#neu_biertyp_name').val()));
 		ws.send(JSON.stringify(myOptions));
 });
 /*Biertyp bearbeiten ausblenden (Abbrechen) */
@@ -33,10 +27,10 @@ $('#button_biertyp3').click(function () {
 
 $.each(myOptions, function(val, text) {
     mySelect.append(
-        $('<option></option>').val(val).html(text)
+        $('<option></option>').val(text).html(text)
     );
 });
 
 $('#select_biertypen1').change(function () {
-		$('#biertyp_message').text($('#select_biertypen1 option:selected').val());
+		/* $('#biertyp_message').text($('#select_biertypen1 option:selected').val()); */
 });
